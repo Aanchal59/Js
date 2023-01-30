fcc490ea64b41b02e485579764e6595dfad014f5
Aanchal59 <aanchalnegi59@gmail.com>
2023-01-31 00:47:33 +0530

    Files added

---
+++ insertAdjacent.js
@@ -0,0 +1,45 @@
+/*
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <div id="first">Hey I am first</div>
+  <script src="script.js"></script>
+
+ <!--
+  This script places a badge on your repl's full-browser view back to your repl's cover
+  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+  teal, blue, blurple, magenta, pink!
+  -->
+  <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+</body>
+
+</html>
+
+
+
+
+------------------------CSS----------------
+html, body {
+  height: 100%;
+  width: 100%;
+}
+
+*/
+
+-------------------------JAvacript--------------------
+
+
+// first.insertAdjacentHTML('beforebegin', '<div class="test">beforebegin</div>');
+// first.insertAdjacentHTML('beforeend', '<div class="test">beforeend</div>');
+// first.insertAdjacentHTML('afterbegin', '<div class="test">afterbegin</div>');
+// first.insertAdjacentHTML('afterend', '<div class="test">afterend</div>');
+first.remove()
